12ac98f18278be4dea157bc8c7eaff7f592e92a1
TylexTy <bulleyt@gmail.com>
2021-04-03 07:24:23 -0400

    possible fix to put method

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-
+app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-app.use(express.json())
+
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
